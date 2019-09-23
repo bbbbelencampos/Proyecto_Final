42e35e6d8ed15ff2d5e6d411c9246a22423c8c48
DennisVAldes <dennis.valdez@anima.edu.uy>
2019-09-23 10:34:50 -0300

    modified:   backup-proyectofinal/bin/.gitignore 	modified:   backup-proyectofinal/src/logica/Manejador.java 	new file:   backup-proyectofinal/src/logica/main.java 	new file:   backup-proyectofinal/src/presentacion/Interface_biblAnima.java

--- backup-proyectofinal/src/logica/Manejador.java
+++ backup-proyectofinal/src/logica/Manejador.java
@@ -1,6 +1,23 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Manejador {
+	private ArrayList<Usuario>usuarios;
+	private ArrayList<Libro>libros;
+	private ArrayList<Prestamo>prestamos;
+
+	private static Manejador instance;
 	
+	private Manejador() {
 	
+	}
+	
+	public static Manejador getInstance() {
+		if(instance == null) {
+			instance = new Manejador();
+		}
+		
+		return instance;
+	}
 }
---
+++ backup-proyectofinal/src/logica/main.java
@@ -0,0 +1,9 @@
+package logica;
+
+public class main {
+
+	public static void main(String[] args) {
+		Manejador man = Manejador.getInstance();
+	}
+
+}
