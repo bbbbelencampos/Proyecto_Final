7bcf61c0c4cc92bbacbdbbe7f602f1c5f551167f
unknown <bbbbelencampos>
2019-10-08 11:15:16 -0300

    modified:   backup-proyectofinal/bin/logica/Manejador.class 	modified:   backup-proyectofinal/bin/logica/Profesor.class

--- backup-proyectofinal/src/logica/Manejador.java
+++ backup-proyectofinal/src/logica/Manejador.java
@@ -3,6 +3,10 @@ package logica;
 import persistencia.*;
 import presentacion.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Manejador {
@@ -25,17 +29,40 @@ public class Manejador {
 	}
 	
 	public void altaUsuario(int id, int CI, String nombre, String apellido, String mail, String password, TipoUsuario tipo, Orientacion orient){
+		Conn connect = new Conn();
+		Connection con = connect.conectarMySQL();
+		Statement s;
 		
 		switch(tipo) {
-		case ESTUDIANTE:
-			Estudiante estudiante = new Estudiante(id, CI, nombre, apellido, mail, password, orient, tipo);
-			break;
-		case PROFESOR:
-			Profesor profesor = new Profesor();
-			break;
-		case BIBLIOTECARIO:
-			
-			break;
+		
+			case ESTUDIANTE:
+				Estudiante estudiante = new Estudiante(id, CI, nombre, apellido, mail, password, orient, tipo);
+				this.usuarios.add(estudiante);
+				
+				try {
+					s = con.createStatement();
+					ResultSet rs = s.executeQuery ("INSERT INTO usuarios(ci, nombre, apellido, mail, pass) VALUES (" +CI +","+ nombre +","+ apellido +"," +mail +","+ password + ","+ orient +")");
+					
+					while(rs.next()) {
+						System.out.println(rs.getString(1) + rs.getString(2));
+					}
+					
+					
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				break;
+			case PROFESOR:
+				Profesor profesor = new Profesor(id, CI, nombre, apellido, mail, password, orient, tipo);
+				this.usuarios.add(profesor);
+				break;
+			case BIBLIOTECARIO:
+				Bibliotecario bibliotecario = new Bibliotecario(id, CI, nombre, apellido, mail, password, tipo);
+				this.usuarios.add(bibliotecario);
+				break;
 		}
 	}
 	
